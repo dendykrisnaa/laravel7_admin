679a3e4474ce96dff9d402ba1ed0fb9b02cee599
Dendy Krisna Ananda <anandadendykrisna851@gmail.com>
2021-04-05 08:06:04 +0700

    mengamankan admin

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'admin'], function(){
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function(){
     Route::get('/','Admin\DashboardController@index');
+
 });
