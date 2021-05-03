73ad19f2be727a1af7013fd6f2cf3b9899ffc616
Dendy Krisna Ananda <anandadendykrisna851@gmail.com>
2021-05-03 15:36:12 +0700

    belajar bareng uts wip

--- resources/views/admin/layout/navigation.blade.php
+++ resources/views/admin/layout/navigation.blade.php
@@ -15,6 +15,7 @@
                         <a href="{{url('/admin')}}"> <i class="menu-icon fa fa-dashboard"></i>Beranda </a>
                         <a href="{{url('/admin/kategori')}}"> <i class="menu-icon fa fa-dashboard"></i>Kategori </a>
                         <a href="{{url('/admin/tugas')}}"> <i class="menu-icon fa fa-dashboard"></i>Tugas </a>
+                        <a href="{{url('/admin/uts')}}"> <i class="menu-icon fa fa-tasks"></i>UTS </a>
                     </li>
                     <h3 class="menu-title">UI elements</h3><!-- /.menu-title -->
                     <li class="menu-item-has-children dropdown">
