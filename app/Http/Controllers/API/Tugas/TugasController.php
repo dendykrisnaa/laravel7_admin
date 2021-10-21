e75881e8342d42d426b69d39c7039cbebf2d37f5
Dendy Krisna Ananda <anandadendykrisna851@gmail.com>
2021-10-22 00:02:05 +0700

    video 3.2.1 controller api create dan update ok

--- app/Http/Controllers/API/Tugas/TugasController.php
+++ app/Http/Controllers/API/Tugas/TugasController.php
@@ -3,9 +3,48 @@
 namespace App\Http\Controllers\API\Tugas;
 
 use App\Http\Controllers\Controller;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TugasController extends Controller
 {
-    //
+    public function store(Request $request){
+        $validateData=$request->validate([
+            'id' => 'required',
+            'nama_tugas' => 'required',
+            'id_kategori' => 'required',
+            'ket_tugas' => 'required',
+            'status_tugas' => 'required',
+        ]);
+
+        $data = new Task;
+        $data->id = $request->id;
+        $data->nama_tugas = $request->nama_tugas;
+        $data->id_kategori = $request->id_kategori;
+        $data->ket_tugas = $request->ket_tugas;
+        $data->status_tugas = $request->status_tugas;
+        $data->save();
+
+        return response()->json($data, 201);
+    }
+
+    public function update(Request $request){
+        $validateData=$request->validate([
+            'id' => 'required',
+            'nama_tugas' => 'required',
+            'id_kategori' => 'required',
+            'ket_tugas' => 'required',
+            'status_tugas' => 'required',
+        ]);
+
+        $data = Task::where('id','=',$request->id)->first();
+        $data->id = $request->id;
+        $data->nama_tugas = $request->nama_tugas;
+        $data->id_kategori = $request->id_kategori;
+        $data->ket_tugas = $request->ket_tugas;
+        $data->status_tugas = $request->status_tugas;
+        $data->save();
+
+        return response()->json($data, 201);
+    }
 }
